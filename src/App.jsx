f98ad74eba463b8a4f34739dfdd2269b1458bf74
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2023-09-22 15:00:23 -0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <nav class="navbar">
+        <div class="navbar-header">
+          <div class="row">
+            <div class="col-12 col">
+              <button class="btn-block">Others brainstorms</button>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div class="row flex-center">
+        <div class="col">New idea+</div>
+      </div>
+    </>
+  )
+}
+
+export default App
